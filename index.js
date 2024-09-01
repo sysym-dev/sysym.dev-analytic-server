65ceae597ae3b6fe1ffaba00be2871276afc098a
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-01 18:54:50 +0700

    server listen

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
-server.post('/pageview/visit', (req) => {
-  console.log(req.body);
-});
+const port = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log('listening 3000');
+server.use(cors());
+
+server.listen(port, () => {
+  console.log(`server listening at ${port}`);
 });
