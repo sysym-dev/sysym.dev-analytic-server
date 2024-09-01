4f0a922524581bad2c5390ec600d250696dbd01f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-01 22:26:08 +0700

    pageview visit validator

--- src/features/pageview/pageview.routes.js
+++ src/features/pageview/pageview.routes.js
@@ -1,16 +1,22 @@
 const { Router } = require('express');
 const { storePageViewVisit } = require('./pageview.controller');
+const { validate } = require('../../cores/validator');
+const { storePageViewVisitBody } = require('./pageview.validator');
 
 const router = Router();
 
-router.post('/api/v1/pageviews/visit', (req, res) => {
-  return res.json(
-    storePageViewVisit({
-      ip: req.ip,
-      userAgent: req.get('user-agent'),
-      body: req.body,
-    }),
-  );
-});
+router.post(
+  '/api/v1/pageviews/visit',
+  validate('body', storePageViewVisitBody),
+  (req, res) => {
+    return res.json(
+      storePageViewVisit({
+        ip: req.ip,
+        userAgent: req.get('user-agent'),
+        body: req.body,
+      }),
+    );
+  },
+);
 
 exports.pageviewRoutes = router;
---
+++ src/features/pageview/pageview.validator.js
@@ -0,0 +1,16 @@
+const { z } = require('zod');
+
+exports.storePageViewVisitBody = z.object({
+  date: z.string().datetime(),
+  lang: z.string(),
+  loadTime: z.number().positive(),
+  page: z.object({
+    title: z.string(),
+    url: z.string().url(),
+    refferer: z.string().url().optional().nullable(),
+  }),
+  screen: z.object({
+    width: z.number().positive(),
+    height: z.number().positive(),
+  }),
+});
