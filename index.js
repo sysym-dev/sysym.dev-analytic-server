ae0431c74a1871b3a2a7c7514b179a54529dd4be
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-01 22:13:19 +0700

    server routes

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv/config');
 
 const express = require('express');
 const cors = require('cors');
+const { routes } = require('./src/routes');
 
 const server = express();
 
 const port = process.env.PORT || 3000;
 
 server.use(cors());
+
+routes.forEach((route) => server.use(route));
 
 server.listen(port, () => {
   console.log(`server listening at ${port}`);
