57ee386c95303d4e60de2b409073340cecda22e4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-02 09:54:28 +0700

    dev parse user agnet

--- src/features/pageview/pageview.controller.js
+++ src/features/pageview/pageview.controller.js
@@ -1,13 +1,16 @@
 const { getGeoFromIp } = require('../../cores/ipgeo');
-const uap = require('ua-parser-js');
+const bowser = require('bowser');
 
 exports.storePageViewVisit = async ({ ip: requestIp, userAgent, body }) => {
   const ip = requestIp === '::1' ? process.env.LOCAL_IP : requestIp;
   const geo = await getGeoFromIp(requestIp);
+  const parsedUserAgent = bowser.parse(userAgent);
 
   return {
     ip,
-    userAgent: uap(userAgent),
+    platform: parsedUserAgent.platform.type,
+    device: parsedUserAgent.os.name,
+    browser: parsedUserAgent.browser.name,
     geo: geo,
   };
 };
