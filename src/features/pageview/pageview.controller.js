153dd1d80aeffc26eeb4bb1717354440e1e6a3fe
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-02 21:31:49 +0700

    pageview create visit

--- src/features/pageview/pageview.controller.js
+++ src/features/pageview/pageview.controller.js
@@ -1,16 +1,27 @@
 const { getGeoFromIp } = require('../../cores/ipgeo');
 const bowser = require('bowser');
+const { PageView } = require('./pageview.model');
 
 exports.storePageViewVisit = async ({ ip: requestIp, userAgent, body }) => {
   const ip = requestIp === '::1' ? process.env.LOCAL_IP : requestIp;
   const geo = await getGeoFromIp(requestIp);
   const parsedUserAgent = bowser.parse(userAgent);
 
-  return {
-    ip,
-    platform: parsedUserAgent.platform.type,
-    device: parsedUserAgent.os.name,
+  return await PageView.create({
     browser: parsedUserAgent.browser.name,
-    geo: geo,
-  };
+    city: geo.city,
+    country: geo.country,
+    device: parsedUserAgent.os.name,
+    ip,
+    lang: body.lang,
+    loadTime: body.loadTime,
+    platform: parsedUserAgent.platform.type,
+    refferer: body.page.refferer,
+    region: geo.region,
+    screenHeight: body.screen.height,
+    screenWidth: body.screen.width,
+    title: body.page.title,
+    url: body.page.url,
+    visitAt: body.visitAt,
+  });
 };
