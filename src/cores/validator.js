dc4a8428ba181f53e42323963ea06dadbf847247
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-03 14:52:40 +0700

    validation error status

--- src/cores/validator.js
+++ src/cores/validator.js
@@ -2,7 +2,7 @@ exports.validate = (path, schema) => async (req, res, next) => {
   const valdiation = await schema.safeParseAsync(req[path]);
 
   if (!valdiation.success) {
-    return res.json(valdiation.error.issues);
+    return res.status(422).json(valdiation.error.issues);
   }
 
   req[path] = valdiation.data;
