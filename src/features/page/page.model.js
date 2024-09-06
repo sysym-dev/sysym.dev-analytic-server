b9944992d487462291dead87f8f24b7adb77b91f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-06 10:15:58 +0700

    page overview

--- src/features/page/page.model.js
+++ src/features/page/page.model.js
@@ -5,6 +5,14 @@ const pageSchema = new mongoose.Schema({
     type: String,
     unique: true,
   },
+  totalViews: {
+    type: Number,
+    default: 1,
+  },
+  totalUniqueViews: {
+    type: Number,
+    default: 1,
+  },
 });
 
 exports.Page = mongoose.model('Page', pageSchema);
