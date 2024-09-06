efcc9f2387111b5dd61a602ab360be467dce2b44
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-06 14:07:17 +0700

    db retry transaction

--- src/cores/db.js
+++ src/cores/db.js
@@ -8,19 +8,5 @@ exports.connect = async () => {
 exports.runInTransaction = async (cb) => {
   const session = await mongoose.startSession();
 
-  session.startTransaction();
-
-  try {
-    const res = await cb(session);
-
-    await session.commitTransaction();
-
-    return res;
-  } catch (err) {
-    await session.abortTransaction();
-
-    throw err;
-  } finally {
-    session.endSession();
-  }
+  return await session.withTransaction(async () => await cb(session));
 };
