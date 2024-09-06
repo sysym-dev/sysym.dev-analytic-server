cc62e686196f0631bd19c74fd8813bb97a3e8b60
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-06 10:16:15 +0700

    pageview session

--- src/features/pageview/pageview.controller.js
+++ src/features/pageview/pageview.controller.js
@@ -2,56 +2,73 @@ const { getGeoFromIp } = require('../../cores/ipgeo');
 const bowser = require('bowser');
 const { PageView } = require('./pageview.model');
 const { findSessionAndUniqueStatus } = require('../session/session.service');
-const { Page } = require('../page/page.model');
+const { updatePageOverview } = require('../page/page.service');
+const { runInTransaction } = require('../../cores/db');
 
 exports.storePageViewVisit = async ({ ip: requestIp, userAgent, body }) => {
   const ip = requestIp === '::1' ? process.env.LOCAL_IP : requestIp;
   const geo = await getGeoFromIp(requestIp);
   const parsedUserAgent = bowser.parse(userAgent);
 
-  const sessionStatus = await findSessionAndUniqueStatus(body.sessionId);
-  const page =
-    (await Page.findOne({ url: body.page.url })) ||
-    (await Page.create({ url: body.page.url }));
+  return await runInTransaction(async (session) => {
+    const pageSessionStatus = await findSessionAndUniqueStatus(body.sessionId, {
+      session,
+    });
+    const page = await updatePageOverview(
+      body.page.url,
+      { uniqe: pageSessionStatus.unique },
+      { session },
+    );
 
-  return await PageView.create({
-    browser: parsedUserAgent.browser.name,
-    city: geo.city,
-    country: geo.country,
-    device: parsedUserAgent.os.name,
-    ip,
-    lang: body.lang,
-    loadTime: body.loadTime,
-    platform: parsedUserAgent.platform.type,
-    refferer: body.page.refferer,
-    region: geo.region,
-    screenHeight: body.screen.height,
-    screenWidth: body.screen.width,
-    title: body.page.title,
-    url: body.page.url,
-    visitAt: body.visitAt,
-    session: sessionStatus.session._id,
-    visitor: sessionStatus.session.visitor,
-    unique: sessionStatus.unique,
-    page: page._id,
+    const [pageView] = await PageView.create(
+      [
+        {
+          browser: parsedUserAgent.browser.name,
+          city: geo.city,
+          country: geo.country,
+          device: parsedUserAgent.os.name,
+          ip,
+          lang: body.lang,
+          loadTime: body.loadTime,
+          platform: parsedUserAgent.platform.type,
+          refferer: body.page.refferer,
+          region: geo.region,
+          screenHeight: body.screen.height,
+          screenWidth: body.screen.width,
+          title: body.page.title,
+          url: body.page.url,
+          visitAt: body.visitAt,
+          session: pageSessionStatus.session._id,
+          visitor: pageSessionStatus.session.visitor,
+          unique: pageSessionStatus.unique,
+          page: page._id,
+        },
+      ],
+      { session },
+    );
+
+    return pageView;
   });
 };
 
 exports.storePageViewLeave = async ({ id, body }) => {
-  const pageView = await PageView.findOneAndUpdate(
-    { _id: id, leaveAt: null },
-    { duration: body.duration, bounce: body.bounce, leaveAt: body.leaveAt },
-  ).populate('session');
+  return await runInTransaction(async (session) => {
+    const pageView = await PageView.findOneAndUpdate(
+      { _id: id, leaveAt: null },
+      { duration: body.duration, bounce: body.bounce, leaveAt: body.leaveAt },
+      { session },
+    ).populate('session');
 
-  if (!pageView) {
-    return null;
-  }
+    if (!pageView) {
+      return null;
+    }
 
-  pageView.session.lastLeaveAt = new Date();
+    pageView.session.lastLeaveAt = new Date();
 
-  await pageView.session.save();
+    await pageView.session.save({ session });
 
-  pageView.depopulate('session');
+    pageView.depopulate('session');
 
-  return pageView;
+    return pageView;
+  });
 };
