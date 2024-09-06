bdfcab2c26dcc1ef6a400ea3300f86472642192b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-06 10:15:51 +0700

    run intransaction helper and set debug to true

--- src/cores/db.js
+++ src/cores/db.js
@@ -1,3 +1,26 @@
 const mongoose = require('mongoose');
 
-exports.connect = async () => await mongoose.connect(process.env.DB_URL);
+exports.connect = async () => {
+  mongoose.set('debug', true);
+
+  await mongoose.connect(process.env.DB_URL);
+};
+exports.runInTransaction = async (cb) => {
+  const session = await mongoose.startSession();
+
+  session.startTransaction();
+
+  try {
+    const res = await cb(session);
+
+    await session.commitTransaction();
+
+    return res;
+  } catch (err) {
+    await session.abortTransaction();
+
+    throw err;
+  } finally {
+    session.endSession();
+  }
+};
