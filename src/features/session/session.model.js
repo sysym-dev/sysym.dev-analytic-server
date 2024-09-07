dcb8740d9ba8579e1205bdff3b7cb0cd2f8f33f6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-07 11:16:42 +0700

    remove session unused

--- src/features/session/session.model.js
+++ src/features/session/session.model.js
@@ -1,23 +1,12 @@
 const mongoose = require('mongoose');
-const dayjs = require('dayjs');
 
-const sessionSchema = new mongoose.Schema({
-  startAt: Date,
-  lastVisitAt: Date,
-  lastLeaveAt: Date,
-  unique: Boolean,
-  visitor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Visitor',
+const sessionSchema = new mongoose.Schema(
+  {
+    startAt: Date,
+    expireAt: Date,
+    lastLeaveAt: Date,
   },
-  unique: {
-    type: Boolean,
-    default: true,
-  },
-});
-
-sessionSchema.virtual('expired').get(function () {
-  return dayjs(new Date()).diff(dayjs(this.lastVisitAt)) > 1000 * 60 * 30;
-});
+  { timestamps: true },
+);
 
 exports.Session = mongoose.model('Session', sessionSchema);
