ce5cff5cb2b53dfb2e828f729aad9bc9c4c1e420
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-08 10:51:18 +0700

    page average load time

--- src/features/page/page.controller.js
+++ src/features/page/page.controller.js
@@ -7,8 +7,9 @@ exports.getPages = async () =>
         _id: '$path',
         totalViews: { $sum: 1 },
         totalUniqueViews: { $sum: { $cond: ['$unique', 1, 0] } },
-        averageDuration: { $avg: '$duration' },
         totalBounce: { $sum: { $cond: ['$bounce', 1, 0] } },
+        averageDuration: { $avg: '$duration' },
+        averageLoadTime: { $avg: '$loadTime' },
       },
     },
     {
@@ -16,8 +17,9 @@ exports.getPages = async () =>
         path: '$_id',
         totalViews: 1,
         totalUniqueViews: 1,
-        averageDuration: 1,
         totalBounce: 1,
+        averageDuration: 1,
+        averageLoadTime: 1,
       },
     },
   ]);
