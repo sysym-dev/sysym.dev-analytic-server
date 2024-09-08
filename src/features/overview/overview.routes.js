dbd522adc141b3620a57eb292f2a6d4ed7f30a60
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-08 12:43:12 +0700

    countries summary

--- src/features/overview/overview.routes.js
+++ src/features/overview/overview.routes.js
@@ -1,12 +1,29 @@
 const { Router } = require('express');
 const { responseJson } = require('../../cores/response');
-const { getOverview } = require('./overview.controller');
+const {
+  getOverview,
+  getPages,
+  getSources,
+  getCountries,
+} = require('./overview.controller');
 
 const router = Router();
 
 router.get(
   '/api/v1/overview',
   responseJson(async () => await getOverview()),
+);
+router.get(
+  '/api/v1/overview/pages',
+  responseJson(async () => await getPages()),
+);
+router.get(
+  '/api/v1/overview/sources',
+  responseJson(async () => await getSources()),
+);
+router.get(
+  '/api/v1/overview/countries',
+  responseJson(async () => await getCountries()),
 );
 
 exports.overviewRoutes = router;
