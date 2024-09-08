9483ead61f565b619e5ad0287e0fe157f02f227b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-08 12:43:19 +0700

    move to oeverview

--- src/features/source/source.controller.js
+++
@@ -1,8 +0,0 @@
-const { PageView } = require('../pageview/pageview.model');
-
-exports.getSources = async () =>
-  await PageView.aggregate([
-    { $group: { _id: '$refferer', total: { $sum: 1 } } },
-    { $project: { url: '$_id', total: 1 } },
-    { $sort: { total: -1 } },
-  ]);
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,4 @@
 const { overviewRoutes } = require('./features/overview/overview.routes');
-const { pageRoutes } = require('./features/page/page.routes');
 const { pageviewRoutes } = require('./features/pageview/pageview.routes');
-const { sourceRoutes } = require('./features/source/source.routes');
 
-exports.routes = [pageviewRoutes, overviewRoutes, pageRoutes, sourceRoutes];
+exports.routes = [pageviewRoutes, overviewRoutes];
